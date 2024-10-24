555108032c8edfe63cc799a29687cfdf7dbe6971
Abhijeet Dhekane <abhijeetdhekane2002@gmail.com>
2024-10-24 15:54:44 +0530

    project done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.sql.*;
 import java.sql.PreparedStatement.*;
-import  java.sql.Connection.*;
+import java.sql.Connection.*;
 import java.sql.DriverManager.*;
 import java.sql.ResultSet.*;
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/hotel";
@@ -17,7 +19,20 @@ public class Main {
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
+        Scanner sc = new Scanner(System.in);
+        //String sql = "insert into reservation
+        // (Reservation_id,Guest_name,Room_number,Contact_number,Reservation_Date)
+        // values(?,?,?,?,?);";
+        try {
+            Connection con = DriverManager.getConnection(url, user, pass);
+            PreparedStatement ps = con.prepareStatement("sql");
 
-        
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
